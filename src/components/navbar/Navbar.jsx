ad1682ac01524a5eaabbd761e6227a9552e15edd
Bakpo Godwin <godwinfriday31@gmail.com>
2023-10-02 19:51:53 +0100

    second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import AuthLinks from "../authLinks/AuthLinks";
-// import ThemeToggle from "../themeToggle/ThemeToggle";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
    return (
@@ -15,13 +15,13 @@ const Navbar = () => {
             <Image src="/youtube.png" alt="youtube" width={24} height={24} />
          </div>
          <div className={styles.logo}>Codivo Blog</div>
-         {/* <div className={styles.links}>
+         <div className={styles.links}>
             <ThemeToggle />
             <Link href="/" className={styles.link}>Homepage</Link>
             <Link href="/" className={styles.link}>Contact</Link>
             <Link href="/" className={styles.link}>About</Link>
             <AuthLinks />
-         </div> */}
+         </div>
       </div>
    );
 };
